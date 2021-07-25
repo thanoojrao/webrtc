17e94dbf60ed6fe0c12deaa37ea990aeaf971a8c
thanoojrao <thanoojlingampally@gmail.com>
2021-07-25 22:52:31 +0530

    creating users

--- createdb.js
+++ createdb.js
@@ -8,17 +8,10 @@ const pool = new Pool({
         rejectUnauthorized:false
     }
 })
-pool.query('DROP TABLE USERS;')
 pool.query('CREATE TABLE USERS(ID SERIAL,mailID VARCHAR(50) PRIMARY KEY,password CHAR(60))',(error,result)=>{
     if(error){
         throw error
     }
-    pool.query('CREATE TABLE ROOMS(ID CHAR(36) PRIMARY KEY,adminID INT,CONSTRAINT FK_AdminRoom FOREIGN KEY (adminID) REFERENCES USERS (ID))',(err,results)=>{
-        if(err){
-            throw err
-        }
-        pool.end(console.log('pool ended successfully'))
-    })
 })
 
   
