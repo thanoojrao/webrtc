c5c30f585f96ce1317c099342ca83a72f884bbfa
thanoojrao <thanoojlingampally@gmail.com>
2021-07-25 15:20:32 +0530

    change revrt

--- createdb.js
+++ createdb.js
@@ -1,18 +1,21 @@
 const Pool = require('pg').Pool
 const dotenv = require('dotenv')
 dotenv.config()
-DB_URL=process.env.DATABASE_URL
 const pool = new Pool({
-    DB_URL,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT),
 })
 
 pool.query('CREATE TABLE USERS(ID SERIAL,mailID VARCHAR(50) PRIMARY KEY,password VARCHAR(30))',(error,result)=>{
     if(error){
         throw error
     }
-    pool.query('CREATE TABLE ROOMS(ID CHAR(36) PRIMARY KEY,adminID INT)',(error,result)=>{
-        if(error){
-            throw error
+    pool.query('CREATE TABLE ROOMS(ID CHAR(36) PRIMARY KEY,adminID INT,CONSTRAINT FK_AdminRoom FOREIGN KEY (adminID) REFERENCES USERS (ID))',(err,results)=>{
+        if(err){
+            throw err
         }
         pool.end(console.log('pool ended successfully'))
     })
