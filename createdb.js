9fb77a3ab0621ff33f7932a94291c62954d0e9b2
thanoojrao <thanoojlingampally@gmail.com>
2021-07-25 14:55:23 +0530

    'changed'

--- createdb.js
+++ createdb.js
@@ -1,12 +1,9 @@
 const Pool = require('pg').Pool
 const dotenv = require('dotenv')
 dotenv.config()
+DB_URL=process.env.DATABASE_URL
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: Number(process.env.DATABASE_PORT),
+    DB_URL,
 })
 
 pool.query('CREATE TABLE USERS(ID SERIAL,mailID VARCHAR(50) PRIMARY KEY,password VARCHAR(30))',(error,result)=>{
--- db_queries.js
+++ db_queries.js
@@ -4,12 +4,10 @@ const { v4: uuidV4 } = require('uuid')
 const Pool = require('pg').Pool
 
 dotenv.config()
+
+DB_URL=process.env.DATABASE_URL
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: Number(process.env.DATABASE_PORT),
+    DB_URL,
 })
 
 const addUser = (req,res)=>{
