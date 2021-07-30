74b3976825a88a01ae11d2f4a0e3718a42d2bd3e
thanoojrao <thanoojlingampally@gmail.com>
2021-07-30 18:10:21 +0530

    change to adminjs not uses blazeface

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,4 @@
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 console.log(USER_ID)
@@ -14,42 +15,28 @@ navigator.mediaDevices.getUserMedia({
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream);
+      addVideoStream(video, userVideoStream)
     })
   })
-
   socket.on('user-connected', userId => {
-    addUser(userId)
     const fc = () => connectToNewUser(userId, stream)
     let timerid = setTimeout(fc, 1000 )
     })
 })
-
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close();
 })
-
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
 })
-
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
-  const userTile = document.createElement('div')
-  userTile.className = 'usertile'
-  userTile.id = `${userId}`
-  const p = document.createElement('p')
-  const text = document.createTextNode(userId)
-  p.appendChild(text)
-  userTile.appendChild(p)
-  /*
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
-  */
   call.on('close', () => {
-    userTile.remove()
+    video.remove()
   })
 
   peers[userId] = call
@@ -63,14 +50,4 @@ function addVideoStream(video, stream) {
   videoGrid.append(video)
 }
 
-function addUser(userId){
-    const userTile = document.createElement('div')
-    userTile.className = 'usertile'
-    userTile.id = `${userId}`
-    const p = document.createElement('p')
-    const text = document.createTextNode(userId)
-    p.appendChild(text)
-    userTile.appendChild(p)
-    videoGrid.append(userTile)
-}
 
