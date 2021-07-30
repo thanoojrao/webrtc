5085be03cfe3e89bab1a07a58f419be071d28de8
thanoojrao <thanoojlingampally@gmail.com>
2021-07-30 18:46:19 +0530

    change to admin files

--- public/admin.js
+++ public/admin.js
@@ -1,4 +1,3 @@
-
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 console.log(USER_ID)
@@ -51,3 +50,61 @@ function addVideoStream(video, stream) {
 }
 
 
+
+//model on stream
+let model,faces,noUser=false,t=0,noT=0;
+
+async function renderPrediction(){
+  const notificationSound = '<source src="./notification_sound.mp3" type="audio/mpeg">';
+  faces =0;
+  const returnTensors = false;
+  const flipHorizontal = false;
+  const annotateBoxes = true;
+  const predictions = await model.estimateFaces(
+    myVideo, returnTensors, flipHorizontal, annotateBoxes);
+
+  if (predictions.length > 0) {
+
+    for (let i = 0; i < predictions.length; i++) {
+      if (returnTensors) {
+        predictions[i].topLeft = predictions[i].topLeft.arraySync();
+        predictions[i].bottomRight = predictions[i].bottomRight.arraySync();
+        if (annotateBoxes) {
+          predictions[i].landmarks = predictions[i].landmarks.arraySync();
+        }
+      }
+      faces +=1;
+      if (annotateBoxes) {
+        const landmarks = predictions[i].landmarks;
+        for (let j = 0; j < landmarks.length; j++) {
+          const x = landmarks[j][0];
+          const y = landmarks[j][1];
+        }
+      }
+    }
+  }
+  console.log(faces);
+  if(faces==0){
+    noUser=true;
+    noT+=5
+    console.log('no user');
+    if(noT==60){
+      console.log('are you here')
+      let audio = document.getElementById('sound')
+      audio.innerHTML='<audio autoplay="autoplay">'+notificationSound+'</audio>'
+    }
+  }
+  else if(faces>0){
+    noUser=false;
+    t=t+5;
+    noT=0;
+    socket.emit('facedata',t);
+  }
+};
+const setupModel = async () => {
+  model = await blazeface.load();
+  setInterval(renderPrediction,5000);
+};
+myVideo.addEventListener('loadedmetadata', () => {
+  setupModel();
+})
